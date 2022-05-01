695b63d8b36e3477d7b4910c816c7a87d2fac4ee
Justin Le <jule@csumb.edu>
2022-05-01 15:34:10 -0700

    single video download works, workaround needed (check header comment)

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template, redirect, url_for, request
+# Uses https://stackoverflow.com/questions/68945080/pytube-exceptions-regexmatcherror-get-throttling-function-name-could-not-find?answertab=modifieddesc#tab-top
+# Above link gets around get throttling function_name could not find match for multiple
+# Go to your env folder into /lib/pythonX.X/site-packages/pytube and you will find the mentioned file from the SO post.
+from flask import Flask, render_template, redirect, url_for, request, send_file
+from io import BytesIO
+from pytube import YouTube
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
@@ -32,14 +37,24 @@ def home():
     form = query()
     if form.validate_on_submit():
         update_url(form.url.data) 
-        return redirect('/download/playlist')
+        return redirect(url_for('download_playlist'), code=307)
     return render_template("home.html", form=form)
 
 # download playlist route
-@app.route('/download/playlist')
+@app.route('/download/playlist' , methods=['GET', 'POST'])
 def download_playlist():
-    print(f'url: {stored_url}')
-    return render_template('result.html', video=stored_url[0])
+    print(f"method: {request.method}")
+    if request.method == "POST":
+        buffer = BytesIO() # Declaring the buffer
+        url = YouTube(stored_url[0]) # Getting the URL
+        #itag = request.form.get("itag") # Get the video resolution 
+        video = url.streams.filter(file_extension='mp4')[0] # Store the video into a variable
+        video.stream_to_buffer(buffer)
+        buffer.seek(0)
+        return send_file(buffer, as_attachment=True, download_name="Video - YT2Video.mp4", mimetype="video/mp4")
+    return redirect(url_for("home"))
+
+    return render_template('result.html', link=stored_url[0])
 
 
 @app.route('/login', methods=['GET', 'POST'])
